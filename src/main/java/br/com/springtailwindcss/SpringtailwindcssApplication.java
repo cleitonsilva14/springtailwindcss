9c7c1455430752b2ad45d3df547affe85fec8933
cleitonsilva14 <cleiton18ads@gmail.com>
2025-11-29 09:45:01 -0300

    Subindo projeto

---
+++ src/main/java/br/com/springtailwindcss/SpringtailwindcssApplication.java
@@ -0,0 +1,13 @@
+package br.com.springtailwindcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtailwindcssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtailwindcssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springtailwindcss/SpringtailwindcssApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springtailwindcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtailwindcssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
